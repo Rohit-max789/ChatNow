2fd29e719d2741423cbec1f2bb058e25a87e1733
Rohit garg <84027181+Rohit-max789@users.noreply.github.com>
2025-01-15 22:48:55 +0530

    Update Sidebar.jsx

--- frontend/vite-project/src/Home/components/Sidebar.jsx
+++ frontend/vite-project/src/Home/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBackSharp } from "react-icons/io5";
 import { BiLogOut } from "react-icons/bi";
-import userConversation from "../../Zustand/useConversation";
+import userConversation from "../../zustand/useConversation";
 import { useSocketContext } from "../../context/SocketContext";
 
 const Sidebar = ({ onSelectUser }) => {
